d8493093b1f6f40d698cfe84a4104f47595aebb6
Alexander Bick <AlexanderBick@me.com>
2023-07-04 11:15:53 +0200

    create products works

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -6,6 +6,15 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const products = await Product.find();
-    return response.status(200).json(products);
+    response.status(200).json(products);
+  } else if (request.method === "POST") {
+    try {
+      const productData = request.body;
+      await Product.create(productData);
+
+      response.status(201).json({ message: "Product created" });
+    } catch (error) {
+      response.status(400).json({ message: error.message });
+    }
   }
 }
